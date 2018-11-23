67434d7eed3b18f777281704636d20a00270cf1b
fukuchiharuki <fukuchi.haruki@gmail.com>
2018-11-23 15:38:32 +0900

    feat: 3.1.2 ESLintのエラーに合わせてコード修正

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,11 @@
 export class Hello {
   constructor(name) {
-    this.say(name);
+    this.name = name;
+    this.say();
   }
 
-  say(name) {
-    console.log(`Hello ${name} World!`);
+  say() {
+    console.log(`Hello ${this.name} World!`);
   }
 }
 
