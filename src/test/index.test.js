db80001c115d64eb8450209388f826814ae5a8be
fukuchiharuki <fukuchi.haruki@gmail.com>
2018-11-30 22:15:54 +0900

    feat: 7.1 console.log()の出力をテストする

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -11,4 +11,12 @@ describe('Hello clas test', () => {
   test('We can check if the name define the class constructor', () => {
     expect(hello.name).toBe(name);
   });
+
+  test('We can check if console.log() called on say() method', () => {
+    const spy = jest.spyOn(console, 'log');
+    hello.say();
+    expect(spy).toHaveBeenCalledWith(`Hello ${name} World!`);
+    spy.mockReset();
+    spy.mockRestore();
+  });
 });
