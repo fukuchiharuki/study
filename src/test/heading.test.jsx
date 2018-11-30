c41ac4717a3e65a3e74d67e1516baf26db933b4b
fukuchiharuki <fukuchi.haruki@gmail.com>
2018-11-30 22:38:39 +0900

    feat: 7.2 Reactコンポーネントをテストするコード

---
+++ src/test/heading.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Header from '../js/heading';
+
+describe('<Header>', () => {
+  test('renders correctly', () => {
+    const name = 'Snapshot';
+    const props = { name };
+    const tree = renderer.create(<Header {...props} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
