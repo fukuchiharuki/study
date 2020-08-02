2bd8ce7a3c00110b0148f4edea2dee832e008d64
fukuchiharuki <fukuchi.haruki@gmail.com>
2020-08-02 14:14:37 +0900

    feat: useEffectを使う

--- src/Example.tsx
+++ src/Example.tsx
@@ -1,7 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Example() {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
 
   return (
     <div>
