6aec7b187e366925cbcbac77ad9c4e8c089887d4
fukuchiharuki <fukuchi.haruki@gmail.com>
2020-08-02 15:28:47 +0900

    feat: FriendStatusを表示

--- src/Example.tsx
+++ src/Example.tsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import FriendStatus from './FriendStatus';
+import ChatAPI from './ChatAPI';
 
 function Example() {
   const [count, setCount] = useState(0);
@@ -12,6 +14,16 @@ function Example() {
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>
         Click me
+      </button>
+      <hr />
+      <p>
+        <FriendStatus friend={{ id: "fukuchi" }} />
+      </p>
+      <button onClick={() => ChatAPI.login("fukuchi")}>
+        Login
+      </button>
+      <button onClick={() => ChatAPI.logout("fukuchi")}>
+        Logout
       </button>
     </div>
   );
