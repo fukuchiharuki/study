ce922bcd21419e5877780de758020485a17385a3
fukuchiharuki <fukuchi.haruki@gmail.com>
2020-08-20 08:46:03 +0900

    feat: some of basic types

--- server.js
+++ server.js
@@ -4,14 +4,19 @@ const { buildSchema } = require("graphql");
 
 const schema = buildSchema(`
   type Query {
-    hello: String
+    quoteOfTheDay: String
+    random: Float!
+    rollThreeDice: [Int]
   }
 `);
 
 const rootValue = {
-  hello: () => {
-    return 'Hello world!';
-  },
+  quoteOfTheDay: () => Math.random() < 0.5
+    ? "Take it easy"
+    : "Salvation lies within",
+  random: () => Math.random(),
+  rollThreeDice: () => [1, 2, 3]
+    .map(_ => 1 + Math.floor(Math.random() * 6)),
 };
 
 const app = express();
