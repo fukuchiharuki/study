07b8e00f6cb1cfd82b3f6c56aff539272ca53787
fukuchiharuki <fukuchi.haruki@gmail.com>
2020-08-21 08:41:27 +0900

    fix: 不要require？

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
-const { argsToArgsConfig } = require("graphql/type/definition");
 
 const schema = buildSchema(`
   type Query {
